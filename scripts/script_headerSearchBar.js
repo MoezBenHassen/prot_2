cf4f62ddd22c65f563ed0e6ac7c4d1a54ed7c7f5
Zeldariam <moez.uzumaki@gmail.com>
2020-04-18 03:05:00 +0200

    did the background wave effect // working on aside menu with underline animation on hover

--- scripts/script_headerSearchBar.js
+++ scripts/script_headerSearchBar.js
@@ -6,3 +6,11 @@ $("#inpt_search").on('blur', function () {
     if ($(this).val().length == 0)
         $(this).parent('label').removeClass('active');
 });
+
+$(document).ready(function () {
+
+    AOS.init({
+        easing: 'ease ',
+        duration: 1800
+    });
+});  
